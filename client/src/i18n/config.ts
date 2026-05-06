13aeaec7ef182055b8ba2a8dec079827f3c4ea6f
ArielShadrac <arielshadrac@gmail.com>
2026-05-06 04:27:19 +0000

    feat: add new file client/src/i18n/config.ts

---
+++ client/src/i18n/config.ts
@@ -0,0 +1,37 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Import des fichiers de traduction
+import frTranslation from './locales/fr.json';
+import enTranslation from './locales/en.json';
+import swTranslation from './locales/sw.json';
+import mosTranslation from './locales/mos.json';
+import haTranslation from './locales/ha.json';
+import woTranslation from './locales/wo.json';
+
+const resources = {
+  fr: { translation: frTranslation },
+  en: { translation: enTranslation },
+  sw: { translation: swTranslation },
+  mos: { translation: mosTranslation },
+  ha: { translation: haTranslation },
+  wo: { translation: woTranslation },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'fr',
+    interpolation: {
+      escapeValue: false, 
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
